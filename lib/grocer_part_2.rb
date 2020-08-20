4715b05b582984a2ae7804d283456ee77fe7a758
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-20 20:15:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -19,11 +19,14 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  cart.each do |key|
-    if key[:clearance]
-      key[:price] *= 0.8
-    end
+  counter = 0 
+  while counter < cart.length
+    if cart[counter][:clearance]
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
+    end 
+    counter += 1 
   end
+  cart
 end
 
 def checkout(cart, coupons)
